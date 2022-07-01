a3654296b6fc704b250eb4e1da9377cb808c9896
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-07-01 16:45:51 -0300

    Add about me section and scroll animation

--- js/script.js
+++ js/script.js
@@ -4,3 +4,23 @@ let typed = new Typed(".auto-input", {
     backSpeed: 50,
     loop: true
 });
+
+function reveal() {
+    let revelations = document.querySelectorAll(".reveal");
+    
+    revelations.forEach((revelation) => {
+        let windowHeight = window.innerHeight;
+        let elementTop = revelation.getBoundingClientRect().top;
+        let elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            revelation.classList.add('active');
+        } else {
+            revelation.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', reveal);
+
+reveal();
